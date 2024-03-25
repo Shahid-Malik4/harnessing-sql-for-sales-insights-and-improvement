dbbb503a6da4fb88fbacf22d34d4b871fa0a59bf
Shahid Malik <79898163+shahidmalik4@users.noreply.github.com>
2024-03-25 17:23:03 +0500

    Update best_selling_items.sql

--- scripts/best_selling_items.sql
+++ scripts/best_selling_items.sql
@@ -13,5 +13,16 @@ ORDER BY total_amount DESC
 LIMIT 10;
 
 /*
-
+| Item Name                  | Total Amount   |
+|----------------------------|----------------|
+| Eco-Friendly Sticky Notes  | 49390909.0000  |
+| Fine Point Ballpoint Pens  | 44105656.2500  |
+| Mechanical Pencils         | 37467015.4000  |
+| Highlighter Markers        | 34488116.9000  |
+| Correction Tape            | 24883336.6400  |
+| Fountain Pens              | 23802428.6700  |
+| Wooden Pencils             | 23528190.6900  |
+| Gel Ink Pens               | 21857564.5000  |
+| Whiteboard Markers         | 21678563.2800  |
+| Erasers                    | 20326873.3100  |
 */

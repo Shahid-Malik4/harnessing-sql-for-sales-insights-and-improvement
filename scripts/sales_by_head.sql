c127edde2247661aaf946cc670575c83d6aff132
Shahid Malik <79898163+shahidmalik4@users.noreply.github.com>
2024-03-25 17:30:35 +0500

    Update sales_by_head.sql

--- scripts/sales_by_head.sql
+++ scripts/sales_by_head.sql
@@ -7,5 +7,15 @@ GROUP BY head
 ORDER BY amount DESC;
 
 /*
+| Region          | Amount      |
+|-----------------|-------------|
+| Islamabad       | 479464455   |
+| Karachi         | 355822085   |
+| Lahore          | 347722844   |
+| Multan          | 215087187   |
+| Faisalabad      | 128248464   |
+| Sargodha        | 95564554    |
+| Bahawalpur      | 21008121    |
+| Peshawar        | 274560      |
 
 */

e000c9832517dea45fbd25500dd47b862318758d
Shahid Malik <79898163+shahidmalik4@users.noreply.github.com>
2024-03-25 17:38:47 +0500

    Update top_companies.sql

--- scripts/top_companies.sql
+++ scripts/top_companies.sql
@@ -9,4 +9,17 @@ ORDER BY total_sales DESC
 LIMIT 10;
 
 /*
+| Company      | Total Sales   |
+|--------------|---------------|
+| Staples      | 249456994.7900|
+| Office Depot | 147250787.8400|
+| WHSmith      | 142250078.3300|
+| Paperchase   | 102761960.5900|
+| Ryman        | 66282793.7300 |
+| Walmart      | 64940850.9100 |
+| Best Buy     | 52973819.4800 |
+| Amazon       | 52661902.0000 |
+| Target       | 46306107.8200 |
+| OfficeMax    | 39016478.8500 |
+
 */
